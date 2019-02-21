0b3628da6ea36cfb289da641b943f1e74c51844c
Brandon Desselle <desselle.brandon2@gmail.com>
2019-02-21 02:41:19 -0600

    Commenting and Colors

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,19 +59,19 @@
 				visibleClass: 'is-menu-visible'
 			});
 
-	// Header.
+/* 	// Header.
 		if ($banner.length > 0
 		&&	$header.hasClass('alt')) {
 
 			$window.on('resize', function() { $window.trigger('scroll'); });
 
 			$banner.scrollex({
-				bottom:		$header.outerHeight() + 1,
+				top:		$banner.outerHeight() + 1,
 				terminate:	function() { $header.removeClass('alt'); },
 				enter:		function() { $header.addClass('alt'); },
 				leave:		function() { $header.removeClass('alt'); }
 			});
 
-		}
+		} */
 
 })(jQuery);
